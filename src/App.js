db9f37a664aeba48d1f3cc501ae3e584dec65320
jcobarreto <julio.barreto@gmail.com>
2020-10-31 16:43:44 -0300

    Add, Delete and List repositories implementation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', { 
+      title: 'Desafio NodeJS',
+      url: 'https://github.com/jcobarreto/conceitos-nodejs',
+      techs: ['Node.js', 'ReactJS']
+    });
+
+    setRepositories([ ...repositories, response.data ]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);
+
+    setRepositories(repositories.filter(repository => repository.id !== id));
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
+        {repositories.map(repository => (
+          <li key={repository.id}>
+            {repository.title}
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
         </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
